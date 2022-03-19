1176817bcb80b704015a27f5c8ed09cf4f737944
hwang <franz0406@naver.com>
2022-03-20 06:34:32 +0900

    logo component modified

--- src/subComponents/LogoComponent.js
+++ src/subComponents/LogoComponent.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import styled from 'styled-components';
+import { darkTheme } from '../styles/Themes';
 
 const Logo = styled.h1`
     position: fixed;
     left: 2rem; top: 2rem;
 
-    color: ${ props => props.theme.text};
-    font-family: 'Pacifico',cursive;
+    color: ${ props => props.color === 'false' ? darkTheme.text : darkTheme.body};
+    transition: 1.5s linear;
+    font-family: 'Source Sans Pro',sans-serif;
     z-index: 3;
 `
-const LogoComponent = () => {
+const LogoComponent = ({theme}) => {
     return (
-        <Logo>CB</Logo>
+        <Logo color={theme.toString()}>Portfolio</Logo>
     );
 };
 
