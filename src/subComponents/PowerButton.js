5194b2a65135a2a6158e1e388531f640f086ee19
hwang <franz0406@naver.com>
2022-03-20 06:35:01 +0900

    powerbutton component modified

--- src/subComponents/PowerButton.js
+++ src/subComponents/PowerButton.js
@@ -8,7 +8,7 @@ import { PowerBtn } from '../components/AllSvgs';
 const Power = styled.button`
     position: fixed;
     top: 2rem; left: 50%;
-    transform: translateX(-50%);
+    transform: translateX(-50%) scale(1);
 
     display: flex;
     justify-content: center;
@@ -21,10 +21,9 @@ const Power = styled.button`
     border: 2px solid #000;
     cursor: pointer;
     z-index: 3;
-    transition: 0.3s ease-in-out;
+    transition: 0.1s ease-in-out;
     &:hover {
-        background-color: rgba(249,255,33,0.4);
-        box-shadow: 0 0 8px 6px rgba(249,255,33,0.2);
+        transform: translateX(-50%) scale(1.1);
     }
     &>*:first-child {
         text-decoration:none;
