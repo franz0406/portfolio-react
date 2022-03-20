52630997382282ce6424261dfeb44dd97460a1b6
hwang <franz0406@naver.com>
2022-03-20 18:44:15 +0900

    intro section add

---
+++ src/components/Intro.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Box = styled.div`
+    position: absolute;
+    top: 50%; left: 50%;
+    transform: translate(-50%,-50%);
+
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 55vw; height:40vh;
+    overflow: hidden;
+
+    background: linear-gradient(
+        to right,
+        ${props => props.theme.body} 50%,
+        ${props => props.theme.text} 50%) top,
+                linear-gradient(
+        to right,
+        ${props => props.theme.body} 50%,
+        ${props => props.theme.text} 50%) bottom;
+    background-size: 100% 3px;
+    background-repeat: no-repeat;    
+    border-left: 3px solid ${props => props.theme.body};           
+    border-right: 3px solid ${props => props.theme.text};
+    z-index:1;
+`
+const BoxContent = styled.div`
+    text-align: center;
+    color:#fff;
+    text-shadow:0 0 10px #000;
+    h3 {
+        padding-bottom: 1vw;
+        font-size: 4.5vw;
+        font-weight: 600;
+    }                
+    @media screen and (max-width:450px){
+        h3 {
+            font-size: 25px;
+        }
+    }
+`
+
+const Intro = () => {
+
+    return (
+        <Box>
+            <BoxContent>
+                <h3>Web Publisher</h3>
+                <p>React Portfolio Website.</p>
+            </BoxContent>
+        </Box>
+    );
+};
+
+export default Intro;
