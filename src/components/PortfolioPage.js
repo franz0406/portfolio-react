134c9ae45903d8c95a579193cc93c64693146e91
hwang <franz0406@naver.com>
2022-03-25 04:47:52 +0900

    porfolio page up

--- src/components/PortfolioPage.js
+++ src/components/PortfolioPage.js
@@ -1,11 +1,43 @@
 import React from 'react';
+import styled, { ThemeProvider } from 'styled-components';
+import { lightTheme } from '../styles/Themes';
+// sub component
+import LogoComponent from '../subComponents/LogoComponent';
+import Particle from '../subComponents/Particle';
+import PowerButton from '../subComponents/PowerButton';
 
-const WorkPage = () => {
+const Container = styled.section`
+    position: relative;
+    background-color: ${props => props.theme.text};
+    width: 100%;
+    min-height: 100vh;
+    > div > div {
+        height: 100%;
+    }
+`
+const Portfolios = styled.ul`
+    position: absolute;
+    top: 50%; left: calc(10rem + 15vw);
+    transform: translateY(-50%);
+    height: 40vh;
+    display: flex;    
+    color: #fff;
+    z-index:3;
+`
+
+const PortfolioPage = () => {
     return (
-        <div>
-            
-        </div>
+        <ThemeProvider theme={lightTheme}>
+            <LogoComponent theme='false'/>
+            <PowerButton />
+            <Container>
+                <Particle theme='dark'/>
+                <Portfolios>
+
+                </Portfolios>
+            </Container>
+        </ThemeProvider>
     );
 };
 
-export default WorkPage;
+export default PortfolioPage;
